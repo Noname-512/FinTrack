c45eb5d1f3ce525e3255c928da8256fa5a89553c
Vivek-512 <thakorvivek512@gmail.com>
2026-01-05 14:07:16 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDNKvqx95B_tVtsd8bEWMdfWD8eALu-X0k",
-  authDomain: "studentbudget-66dcc.firebaseapp.com",
+  authDomain: "fin-track-bay.vercel.app",
   projectId: "studentbudget-66dcc",
   storageBucket: "studentbudget-66dcc.firebasestorage.app",
   messagingSenderId: "398984222542",
@@ -17,4 +17,5 @@ const app = initializeApp(firebaseConfig);
 // 2. Export the specific tools (The 'export' keyword is the most important part!)
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
 export const provider = new GoogleAuthProvider();
